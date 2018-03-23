c064db0f4ccc0b549b70f0c9d2da263afd10d6f8
Juliette PARISSEAUX <juliette.parisseaux@etudiant.univ-lille1.fr>
2018-03-23 14:58:15 +0100

    add page creation event

--- SportIn/app/src/main/java/fr/univ_lille1/iutinfo/sportin/PageCreerEvent.java
+++ SportIn/app/src/main/java/fr/univ_lille1/iutinfo/sportin/PageCreerEvent.java
@@ -1,7 +1,13 @@
 package fr.univ_lille1.iutinfo.sportin;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by magnierb on 23/03/18.
@@ -12,6 +18,22 @@ public class PageCreerEvent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.activity_creer_event);
+    }
+
+    public void creerEvent(View view){
+
+        if(true) { //TODO verifier que l'event est valide
+
+            //TODO creer l'event
+
+            Toast.makeText(getApplicationContext(),"Event crée",Toast.LENGTH_LONG).show();
+
+            Intent i = new Intent(this, PageNav.class);
+            startActivity(i);
+        } else {
+            Toast.makeText(getApplicationContext(),"Event invalide",Toast.LENGTH_LONG).show();
+        }
+
     }
 
 }
