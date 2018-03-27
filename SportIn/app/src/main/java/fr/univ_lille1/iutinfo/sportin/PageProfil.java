00d415e510f31887f1343482026fe3c65a3b8cbb
Juliette PARISSEAUX <juliette.parisseaux@etudiant.univ-lille1.fr>
2018-03-27 10:17:02 +0200

    ça marche bien

--- SportIn/app/src/main/java/fr/univ_lille1/iutinfo/sportin/PageProfil.java
+++ SportIn/app/src/main/java/fr/univ_lille1/iutinfo/sportin/PageProfil.java
@@ -32,14 +32,39 @@ import java.util.ListIterator;
 import fr.univ_lille1.iutinfo.communication.ConnexionServ;
 public class PageProfil extends AppCompatActivity {
 
+    private List<String> listSports;
+    ListView list;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profil);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Initialisation de la liste contenant les sports évalués par l'utilisateur
+        listSports = new ArrayList<>();
+        for(int i = 0;i<5 ; i++){ //TODO le nombre de sports que l'utilisateur à selectionné (à remplacer)
+            String nomSport ="Nom du sport"; //TODO remplacer avec le nom du sport
+            int rating = i; //TODO remplacer avec la bonne valeur
+            String ratingString="";
+            for(int nbEtoiles = 0;nbEtoiles<rating ; nbEtoiles++){
+                ratingString+="*";
+            }
+            listSports.add(nomSport+"     "+ratingString);
+        }
+
+        //Initialisation de la listeView
+        list = (ListView) findViewById(R.id.listView);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(PageProfil.this,
+                android.R.layout.simple_list_item_1, listSports);
+        list.setAdapter(adapter);
     }
 
+    /**
+     * Méthode liée au bouton "Modifier mon compte"
+     * qui doit envoyer vers la page de modification de compte
+     * @param view
+     */
     public void modifierCompte(View view) {
         //TODO envoyer sur la future page de modification de compte
         Intent i=new Intent(this,PageProfil.class);
